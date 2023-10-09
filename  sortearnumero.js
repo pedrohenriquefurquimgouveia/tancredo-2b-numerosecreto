c901abd8757fa1119843984b95dec9fb39178d53
Pedro Henrique Furquim Gouveia <106687665+pedrohenriquefurquimgouveia@users.noreply.github.com>
2023-10-09 12:18:51 -0300

    Add files via upload

---
+++  sortearnumero.js
@@ -0,0 +1,17 @@
+const menorvalor = 1 
+const maiorvalor = 1000
+constnumerosecret  = gerarNumeroAleatorio()
+
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+
+}
+
+console.log('numero Secreto:', numerosecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = maiorValor
